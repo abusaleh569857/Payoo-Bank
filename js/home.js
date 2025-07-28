76e3326168edd079b2d6314e8ecf6cd0fdad7109
Abusaleh Alam Khan <abusaleh.uiu.cse@gmail.com>
2025-07-28 23:37:21 +0600

    Project Setup & Create Simple Login Form

---
+++ js/home.js
@@ -0,0 +1,5 @@
+document.getElementById("login-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("LOgin btn clicked");
+  window.location.href = "/home.html";
+});
